6b0502531969d4e4992afed58e8912a10c30b0ad
Fau <Fau818@qq.com>
2023-01-07 13:32:26 +0800

    feat: use '# %s' to comment dosini filetype, not '; %s'

--- lua/Fau/core/comment.lua
+++ lua/Fau/core/comment.lua
@@ -3,12 +3,22 @@
 -- =============================================
 local comment = Fau_vim.load_plugin("Comment")
 if comment == nil then return end
+local comment_ft = require('Comment.ft')
 
 
 
 -- =============================================
 -- ========== Configuration
 -- =============================================
-comment.setup({
+local config = {
 	pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-})
+}
+
+
+comment.setup(config)
+
+
+-- =============================================
+-- ========== Specific Filetype
+-- =============================================
+comment_ft.set("dosini", "# %s")
