d619df128174c4bd17550e7c2f340dc486adc05e
Fau <Fau818@qq.com>
2023-03-23 14:21:37 +0800

    fix(snippets): fix one acm_cpp snippet refactor(cmp): use default setting for the `performance` option.

--- lua/Fau/core/cmp.lua
+++ lua/Fau/core/cmp.lua
@@ -41,9 +41,9 @@ local config = {
   enable = true,  -- Toggles the plugin on and off.
   snippet = { expand = function(args) luasnip.lsp_expand(args.body) end }, -- for loading custom snippets of luasnip
   performance = {
-    debounce = 100,          -- popup menu delay
-    throttle = 50,           -- refresh delay
-    fetching_timeout = 1000, -- fetching timeout
+    debounce = 60,          -- popup menu delay
+    throttle = 30,           -- refresh delay
+    fetching_timeout = 500, -- fetching timeout
   },
 
   completion = {
