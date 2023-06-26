e7230c7b60cd554789921205ac9e834a62624136
Fau <Fau818@qq.com>
2023-06-26 16:15:40 +0800

    feat(comment): ignore blank lines

--- lua/Fau/core/comment.lua
+++ lua/Fau/core/comment.lua
@@ -17,7 +17,7 @@ local config = {
   ---Whether the cursor should stay at its position
   sticky = true,
   ---Lines to be ignored while (un)comment
-  -- ignore = nil,
+  ignore = "^$",
   ---LHS of toggle mappings in NORMAL mode
   toggler = {
     ---Line-comment toggle keymap
