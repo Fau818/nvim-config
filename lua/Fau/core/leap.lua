686204469346e319d7b89226ca87c154d73a4c06
Fau <Fau818@qq.com>
2023-07-05 10:59:10 +0800

    revert(leap): remove `<ESC>` key to evoke search anywhere

--- lua/Fau/core/leap.lua
+++ lua/Fau/core/leap.lua
@@ -25,18 +25,3 @@ vim.keymap.set(
   end,
   { desc = "Leap Anywhere" }
 )
-
-vim.keymap.set(
-  { "n" }, "<ESC>",
-  function()
-    vim.api.nvim_command("nohlsearch")
-    if vim.bo.filetype == "toggleterm" then return end
-
-    local focusable_windows_on_tabpage = vim.tbl_filter(
-      function(win) return vim.api.nvim_win_get_config(win).focusable end,
-      vim.api.nvim_tabpage_list_wins(0)
-    )
-    require("leap").leap { target_windows = focusable_windows_on_tabpage }
-  end,
-  { desc = "Leap Anywhere" }
-)
