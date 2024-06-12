5c36f5fbee67e723dbe26bd966375f296b4a5aac
Fau <zhangyuFau@gmail.com>
2024-06-12 15:56:16 +0800

    style(lazy): format

--- lua/Fau/lazy.lua
+++ lua/Fau/lazy.lua
@@ -8,11 +8,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",  -- latest stable release
     lazypath,
   })
 end
----@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 
@@ -32,7 +31,7 @@ ViewConfig.keys.hover = "<C-d>"
 -- -----------------------------------
 ---@type LazyConfig
 local config = {
-  root = vim.fn.stdpath("data") .. "/lazy", -- directory where plugins will be installed
+  root = vim.fn.stdpath("data") .. "/lazy",  -- directory where plugins will be installed
   defaults = {
     lazy = false,
     cond = not vim.g.vscode,
@@ -40,15 +39,15 @@ local config = {
 
   -- leave nil when passing the spec as the first argument to setup()
   spec = "Fau.plugins",
-  lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json", -- lockfile generated after running update.
+  lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",  -- lockfile generated after running update.
   ---@type number limit the maximum amount of concurrent tasks
   concurrency = jit.os:find("Windows") and (vim.loop.available_parallelism() * 2) or 50,
 
   git = {
     -- defaults for the `Lazy log` command
     -- log = { "-10" }, -- show the last 10 commits
-    log = { "--since=3 days ago" }, -- show commits from the last 3 days
-    timeout = 120, -- kill processes that take more than 2 minutes
+    log = { "--since=3 days ago" },  -- show commits from the last 3 days
+    timeout = 120,                   -- kill processes that take more than 2 minutes
     url_format = "https://github.com/%s.git",
     filter = true,
   },
@@ -57,8 +56,8 @@ local config = {
     -- directory where you store your local plugin projects
     path = "~/projects",
     ---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
-    patterns = {}, -- For example {"folke"}
-    fallback = false, -- Fallback to git when local plugin doesn't exist
+    patterns = {},     -- For example {"folke"}
+    fallback = false,  -- Fallback to git when local plugin doesn't exist
   },
 
   install = {
@@ -71,23 +70,23 @@ local config = {
   ui = {
     -- a number <1 is a percentage., >1 is a fixed size
     size = { width = 0.9, height = 0.85 },
-    wrap = true, -- wrap the lines in the ui
+    wrap = true,  -- wrap the lines in the ui
     border = "double",
-    title = nil, ---@type string only works when border is not "none"
-    title_pos = "center", ---@type "center" | "left" | "right"
+    title = nil,  ---@type string only works when border is not "none"
+    title_pos = "center",  ---@type "center" | "left" | "right"
     -- Show pills on top of the Lazy window
-    pills = true, ---@type boolean
+    pills = true,  ---@type boolean
     icons = nil,  -- NOTE: Use defaults
     -- leave nil, to automatically select a browser depending on your OS.
     -- If you want to use a specific browser, you can define it here
-    browser = nil, ---@type string?
-    throttle = 20, -- how frequently should the ui process render events
+    browser = nil,  ---@type string?
+    throttle = 20,  -- how frequently should the ui process render events
     custom_keys = {
       -- Open a terminal for the plugin dir
       ["<localleader>D"] = function(plugin) require("lazy.util").float_term(nil, { cwd = plugin.dir }) end,
       -- Open lazygit log
       ["<localleader>L"] = function(plugin) require("lazy.util").float_term({ "lazygit", "log" }, { cwd = plugin.dir }) end,
-    }
+    },
   },
 
   diff = {
@@ -103,25 +102,25 @@ local config = {
   checker = {
     -- automatically check for plugin updates
     enabled = true,
-    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
-    notify = true, -- get a notification when new updates are found
-    frequency = 3600, -- check for updates every hour
+    concurrency = nil,  ---@type number? set to 1 to check for updates very slowly
+    notify = true,     -- get a notification when new updates are found
+    frequency = 3600,  -- check for updates every hour
     check_pinned = false,
   },
 
   change_detection = {
     -- automatically check for config file changes and reload the ui
     enabled = true,
-    notify = true, -- get a notification when changes are found
+    notify = true,  -- get a notification when changes are found
   },
 
   performance = {
     cache = { enabled = true },
-    reset_packpath = true, -- reset the package path to improve startup time
+    reset_packpath = true,  -- reset the package path to improve startup time
     rtp = {
-      reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+      reset = true,  -- reset the runtime path to $VIMRUNTIME and your config directory
       ---@type string[]
-      paths = {}, -- add any custom paths here that you want to includes in the rtp
+      paths = {},  -- add any custom paths here that you want to includes in the rtp
       ---@type string[] list any plugins you want to disable here
       disabled_plugins = {},
     },
@@ -138,7 +137,7 @@ local config = {
     skip_if_doc_exists = true,
   },
 
-  state = vim.fn.stdpath("state") .. "/lazy/state.json", -- state info for checker and other things
+  state = vim.fn.stdpath("state") .. "/lazy/state.json",  -- state info for checker and other things
   build = { warn_on_override = true },
 
   -- Enable profiling of lazy.nvim. This will add some overhead,
