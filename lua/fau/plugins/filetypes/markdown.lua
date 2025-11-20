adba422cdeaa28d0d5a2edfb67f9dcbf961f5a2e
Fau <zhangyuFau@gmail.com>
2025-11-20 18:16:38 -0500

    fix(plugins): correct build condition for markdown-preview.nvim

--- lua/fau/plugins/filetypes/markdown.lua
+++ lua/fau/plugins/filetypes/markdown.lua
@@ -53,7 +53,8 @@ return {
   {
     -- DESC: A markdown previewer using a web browser.
     "iamcco/markdown-preview.nvim",
-    build = vim.fn.executable("yarn") == 1 and "cd app && yarn install" or function() vim.fn["mkdp#util#install"]() end,
+    enabled = vim.fn.executable("yarn") == 1,
+    build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
       vim.g.mkdp_echo_preview_url = 1
