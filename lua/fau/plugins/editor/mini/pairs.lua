e272b92c050bae355759728f322b2f5ac3e1ac49
Fau <zhangyuFau@gmail.com>
2026-06-06 17:05:34 -0400

    feat(plugins)!: remove chatgpt.nvim feat(mini.pairs): disable in recording macros, replace mode, and visual block mode

--- lua/fau/plugins/editor/mini/pairs.lua
+++ lua/fau/plugins/editor/mini/pairs.lua
@@ -4,6 +4,38 @@ return {
   "nvim-mini/mini.pairs",
   lazy = true,  -- Loaded by blink.cmp
 
+  init = function()
+    local augroup = vim.api.nvim_create_augroup("MiniPairsDisable", { clear = true })
+
+    local function recording() return vim.fn.reg_recording() ~= "" end
+
+    -- Disable in recording macros
+    vim.api.nvim_create_autocmd("RecordingEnter", { group = augroup, callback = function() vim.g.minipairs_disable = true end })
+    vim.api.nvim_create_autocmd("RecordingLeave", { group = augroup, callback = function() vim.g.minipairs_disable = false end })
+
+    -- Disable in replace mode
+    vim.api.nvim_create_autocmd("ModeChanged", {
+      group = augroup,
+      pattern = { "n*:R*", "v*:R*", "\22*:R*" },
+      callback = function()
+        if recording() then return end
+        vim.g.minipairs_disable = true
+        vim.api.nvim_create_autocmd("ModeChanged", { group = augroup, pattern = "R*:n*", once = true, callback = function() if recording() then return end vim.g.minipairs_disable = false end })
+      end,
+    })
+
+    -- Disable in visual block mode.
+    vim.api.nvim_create_autocmd("ModeChanged", {
+      group = augroup,
+      pattern = "\22*:i*",
+      callback = function()
+        if recording() then return end
+        vim.g.minipairs_disable = true
+        vim.api.nvim_create_autocmd("ModeChanged", { group = augroup, pattern = "i*:n*", once = true, callback = function() if recording() then return end vim.g.minipairs_disable = false end })
+      end,
+    })
+  end,
+
   opts = {
     -- In which modes mappings from this `config` should be created
     modes = { insert = true, command = false, terminal = false },
@@ -27,9 +59,9 @@ return {
       ["}"] = { action = "close", pair = "{}", neigh_pattern = "^[^\\]" },
 
       -- "^[^%w%-%_\\]"
-      ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "^[%s]", register = { cr = false } },
-      ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "^[%s]", register = { cr = false } },
-      ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "^[%s]", register = { cr = false } },
+      ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "^[^\\%w_]", register = { cr = false } },
+      ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "^[^\\%w_]", register = { cr = false } },
+      ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "^[^\\%w_]", register = { cr = false } },
 
       [" "] = { action = "closeopen", pair = "  ", neigh_pattern = "[%(%[{][%)%]}]" }
     },
