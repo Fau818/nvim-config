e0915490c5e8ee5ef4c968ca1932eb9853f1f496
Fau <zhangyuFau@gmail.com>
2026-06-19 17:42:47 -0400

    feat(todo-comments): multiline pattern should start with a word character

--- lua/fau/plugins/editor/todo-comments.lua
+++ lua/fau/plugins/editor/todo-comments.lua
@@ -38,7 +38,7 @@ return {
     merge_keywords = false,  -- The default keywords are defined manually.
     highlight = {
       multiline         = true,
-      multiline_pattern = [[^.]],
+      multiline_pattern = [[^%w]],
       multiline_context = 10,
 
       before  = "",      ---@type "fg" | "bg" | ""
