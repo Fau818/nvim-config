530c95be0bd8a66753aa5bda1dce96278a1c3db8
Fau <zhangyuFau@gmail.com>
2026-06-23 19:22:37 -0400

    feat(ftplugin): set conceallevel to 0 for jsonc files feat(ftplugin): unlisted grug-far buffers

--- after/ftplugin/grug-far.lua
+++ after/ftplugin/grug-far.lua
@@ -1,1 +1,2 @@
 vim.wo.winfixbuf = true
+vim.bo.buflisted = false
--- after/ftplugin/jsonc.lua
+++ after/ftplugin/jsonc.lua
@@ -2,3 +2,5 @@ if vim.b.fvim_jsonc_ftplugin_loaded then return end
 vim.b.fvim_jsonc_ftplugin_loaded = true
 
 vim.cmd.runtime("after/ftplugin/all.lua")
+
+vim.opt_local.conceallevel = 0
